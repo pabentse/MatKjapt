25ca5143438c6d44e901fbcfdfdb5fe3c607a002
pabentse <pabentse@ifi.uio.no>
2025-01-02 15:02:50 +0100

    a

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ app = FastHTML()
 # Initialize Jinja2 templates
 templates = Jinja2Templates(directory='templates')
 
-os.getenv('OPENAI_API_KEY')
+api_key = os.getenv('OPENAI_API_KEY')
 
-client = OpenAI(api_key='OPENAI_API_KEY')
+client = OpenAI(api_key=api_key)
 
 # Optional: CORS if your site might be called from a different domain
 app.add_middleware(
